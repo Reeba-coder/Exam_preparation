904f1a6a44ac41c13fca7ff43be3e569b6910feb
Reeba <reebarasheedahmed@gmail.com>
2025-06-07 19:48:56 +0500

    Add files via upload

---
+++ 10th.py
@@ -0,0 +1,15 @@
+def gmean(a,b):
+    mean=(a*b)/(a+b)
+    print(mean)
+a=10
+b=7
+gmean(a,b)
+def average(a,b):
+    print("Average of",a , "and", b, "is", (a+b)/2)
+a=10
+b=20
+average(a,b)
+colors = ["Red", "Blue", "Green", "Yellow", "Purple", "Black", "White"]
+for color in colors:
+    print(color)
+print(colors[:4])
